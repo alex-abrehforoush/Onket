676e532f8af2bae5f966ef2bc2a1fa94b0f5b748
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-06-17 20:39:43 +0430

    add my class from Qt0 to my proje in Moein khorsani directory

--- Project/Moein Khorasani/DijiKala/main.cpp
+++ Project/Moein Khorasani/DijiKala/main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QString>
+
+#include "LinkedList.h"
+#include "date.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    LinkedList<int> l;
+    l+=3;
+    Date d;
+    d.goToCurrentDate();
+
+    QString s=d.toString();
+
     w.show();
     return a.exec();
 }
