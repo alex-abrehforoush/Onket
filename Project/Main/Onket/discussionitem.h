2a0a853ad2e6595f0683d92741de243545fae72b
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-07-31 17:06:08 +0430

    remove addView() funciton

--- Project/Main/Onket/discussionitem.h
+++ Project/Main/Onket/discussionitem.h
@@ -28,7 +28,7 @@ public:
     DiscussionItem(const QDate& date_create, const QString& id,const QString& sender_id,const QString& content );
     ~DiscussionItem();
     virtual void addToFile(QXmlStreamWriter & xml_writer)=0;
-    virtual bool addView(const QString& ,bool)=0;
+
 };
 
 #endif // DISCUSSIONITEM_H
