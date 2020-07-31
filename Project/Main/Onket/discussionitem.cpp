151e9bbac9e102dbca21984d5cfeb23c53a8f6f4
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-07-31 10:32:39 +0430

    Write set,get & addToFile() function for discussionItem class

--- Project/Main/Onket/discussionitem.cpp
+++ Project/Main/Onket/discussionitem.cpp
@@ -1,6 +1,35 @@
 #include "discussionitem.h"
 
-DiscussionItem::DiscussionItem()
+void DiscussionItem::setContent(const QString &content)
+{
+    this->content=content;
+}
+
+QString DiscussionItem::getId()
+{
+    return this->id;
+}
+
+QString DiscussionItem::getSenderId()
+{
+    return  this->sender_id;
+}
+
+QString DiscussionItem::getContent()
+{
+    return  this->content;
+}
+
+DiscussionItem::DiscussionItem(const QString &id, const QString &sender_id, const QString &content)
+{
+    this->id=id;
+    this->sender_id=sender_id;
+    this->content=content;
+}
+
+DiscussionItem::~DiscussionItem()
 {
 
 }
+
+
--- Project/Main/Onket/discussionitem.h
+++ Project/Main/Onket/discussionitem.h
@@ -2,11 +2,22 @@
 #define DISCUSSIONITEM_H
 #include <QString>
 #include <QDate>
+#include <QXmlStreamWriter>
 
 class DiscussionItem
 {
+    QString id;
+    QString sender_id;
+    QString content;
 public:
-    DiscussionItem();
+    void setContent(const QString& content);
+
+    QString getId();
+    QString getSenderId();
+    QString getContent();
+    DiscussionItem( const QString& id,const QString& sender_id,const QString& content );
+    ~DiscussionItem();
+    virtual void addToFile(QXmlStreamWriter & xml_writer)=0;
 };
 
 #endif // DISCUSSIONITEM_H
