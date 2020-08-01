2a5178a1c3a9a10a14f88c62d79fa0f88feed431
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-01 17:45:12 +0430

    add user_like and it function to Comment class

--- Project/Main/Onket/comment.cpp
+++ Project/Main/Onket/comment.cpp
@@ -2,6 +2,94 @@
 const QVector<double> Comment::grades={0.0,0.25,0.5,0.75,1.0};
 /////
 
+QString Comment::itemToXml(const QString &item_name)
+{
+    QString resualt;
+   for(auto it=item_name.cbegin(); it!= item_name.cend();it++)
+   {
+       if(*it==' ')
+       {
+           resualt.append("-");
+       }
+       else
+       {
+           resualt.append(*it);
+       }
+   }
+   return  resualt;
+}
+
+QString Comment::xmlToItem(const QString &xml)
+{
+    QString resualt;
+   for(auto it=xml.cbegin(); it!= xml.cend();it++)
+   {
+       if(*it=='-')
+       {
+           resualt.append(" ");
+       }
+       else
+       {
+           resualt.append(*it);
+       }
+   }
+   return  resualt;
+}
+
+QString Comment::toQString(const QMap<QString, bool> &input)
+{
+    if(input.isEmpty()==true)
+    {
+        return QString("");
+    }
+
+    auto it=input.cbegin();
+    QString resault=it.key();
+    resault.append("_");
+    resault.append(QString::number(*it));
+
+    if(it != input.cend())
+        it++;
+    for(;it !=  input.cend();it++)
+    { resault.append(",");
+        resault.append(it.key());
+        resault.append("_");
+        resault.append(QString::number(*it));
+
+    }
+    return resault;
+}
+
+QMap<QString, bool> Comment::toQMap(const QString &input)
+{
+    QMap<QString,bool>resualt;
+    QString sender;
+    for(auto it=input.cbegin();it != input.cend(); it++)
+    {
+        if(*it == '_')
+        {
+            it++;
+            bool mode;
+            if(*it=='0')
+            {
+                mode=false;
+            }
+            else
+            {
+                mode=true;
+            }
+            it++;
+            resualt.insert(sender,mode);
+            sender.clear();
+        }
+        else
+        {
+         sender.append(*it);
+        }
+    }
+    return resualt;
+}
+
 void Comment::setAdvantages(const QString & advantages)
 {
     this->advantages=advantages;
@@ -38,6 +126,54 @@ QString Comment::getDescription()
     return this->description;
 }
 
+bool Comment::find(const QString &sender_id)
+{
+    auto it=users_like.find(sender_id);
+    if (it != users_like.end())
+    {
+        return true;
+    }
+    else return false;
+}
+
+void Comment::addLike(const QString &sender_id)
+{
+    auto it=users_like.find(sender_id);
+    if (it != users_like.end())
+    {
+        users_like[sender_id]=true;
+    }
+    else
+    {
+        users_like.insert(sender_id,true);
+    }
+}
+
+void Comment::addDisLike(const QString &sender_id)
+{
+    auto it=users_like.find(sender_id);
+    if (it != users_like.end())
+    {
+        users_like[sender_id]=false;
+    }
+    else
+    {
+        users_like.insert(sender_id,false);
+    }
+}
+
+bool Comment::getLikeMode(const QString &sender_id)
+{
+    if(this->find(sender_id)==true)
+    {
+        return users_like[sender_id];
+    }
+    else return true;
+}
+
+
+
+
 bool Comment::insertItem(const QString &item_name)
 {
     bool resualt;
@@ -46,8 +182,10 @@ bool Comment::insertItem(const QString &item_name)
     {
         foreach(QChar ch,item_name)
         {
-            if(ch==' ' || ch=='\n')
+            if(ch=='-' || ch=='\n')
                 return false;
+
+
         }
         resualt=true;
         map_items.insert(item_name,0.5);
@@ -128,13 +266,14 @@ void Comment::addToFile(QXmlStreamWriter &xml_writer)
 
         for(auto it=map_items.begin();it != map_items.end();it++)
         {
-            xml_writer.writeAttribute(it.key(),QString::number(it.value()));
+            xml_writer.writeAttribute(itemToXml(it.key()),QString::number(it.value()));
         }
 
        xml_writer.writeTextElement("sender_id",this->sender_id);
         xml_writer.writeTextElement("advantages",this->advantages);
          xml_writer.writeTextElement("disadvantages",this->disadvantages);
        xml_writer.writeTextElement("description",this->description);
+        xml_writer.writeTextElement("users_like",Comment::toQString(this->users_like));
     xml_writer.writeEndElement();
 }
 
--- Project/Main/Onket/comment.h
+++ Project/Main/Onket/comment.h
@@ -11,9 +11,16 @@ class Comment
     static const QVector<double> grades;
     QString sender_id;
     QMap<QString,double>map_items;//the key is item name & the value is item value
+    QMap<QString,bool>users_like;
     QString advantages,disadvantages;
     QString description;
+    QString itemToXml(const QString& item_name);
+    static QString toQString(const QMap<QString,bool>& input);
 public:
+    static QString xmlToItem(const QString& xml);
+
+    static QMap<QString,bool> toQMap(const QString & input);
+
     void setAdvantages(const QString &advantages);
     void setDisAdvantages(const QString & disadvantages);
     void setDescription(const QString& description);
@@ -23,6 +30,11 @@ public:
     QString getAdvantages();
     QString getDisadvantages();
     QString getDescription();
+
+    bool find(const QString& sender_id);
+    void addLike(const QString& sender_id);
+    void addDisLike(const QString& sender_id);
+    bool getLikeMode(const QString& sender_id);
 
     bool insertItem(const QString& item_name);
     bool removeItem(const QString& item_name);
@@ -34,5 +46,7 @@ public:
     void addToFile(QXmlStreamWriter & xml_writer);
 
 };
+
+
 
 #endif // COMMENT_H
