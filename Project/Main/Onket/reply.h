95bf84e84b22bfd776b5c2dd93d7e962af09d037
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-06 13:11:30 +0430

    change replys from QVector to QMultiMap ,add replys_id and change function related to them 2)Write fucntion change reply like

--- Project/Main/Onket/reply.h
+++ Project/Main/Onket/reply.h
@@ -6,20 +6,23 @@
 
 class Reply: public DiscussionItem
 {
+    QString question_id;
     unsigned int like=0,dislike=0;
     QMap<QString,bool>users_like;
 
 public:
 
 
+    QString getQuestionId();
     unsigned int getLikeNumber()const;
     unsigned int getDisLikeNumber()const;
 
 
 
-    explicit Reply(const QDate& date_create, const QString& id,const QString& sender_id,const QString& content);
+    explicit Reply(const QDate& date_create, const QString& id,const QString& sender_id,const QString& question_id,const QString& content);
     void addLike(const QString& sender_id);
     void addDisLike(const QString& sender_id);
+
     bool exist(const QString& sender_id)const;
     bool getLikeMode(const QString& sender_id)const;
 
