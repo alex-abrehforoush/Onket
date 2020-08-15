e5dee2aec4d232be1d6d39bd1f4bdbee7291afd8
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-15 19:42:35 +0430

    add good_id,id_valid,main_lay 2)change constructor

--- Project/Main/Onket/commentitemstatus.h
+++ Project/Main/Onket/commentitemstatus.h
@@ -3,12 +3,19 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QVBoxLayout>
+
+#include "commentitemstatusfield.h"
+#include "good.h"
 
 class CommentItemStatus : public QWidget
 {
     Q_OBJECT
+    QString good_id;
+    bool id_valid=false;
+    QVBoxLayout* main_lay=nullptr;
 public:
-    explicit CommentItemStatus(QWidget *parent = nullptr);
+    explicit CommentItemStatus(const QString& good_id,QWidget *parent = nullptr);
 
 signals:
 
