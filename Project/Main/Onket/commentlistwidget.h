0182b2e4627c13468955ea5473e03f196dcc1334
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-15 07:06:19 +0430

    add center_widget,main_lay,good_id,user_id,id_valid,WIdgets, 2)Write fucntion update existCommentsender,getItem

--- Project/Main/Onket/commentlistwidget.h
+++ Project/Main/Onket/commentlistwidget.h
@@ -2,6 +2,10 @@
 #define COMMENTLISTWIDGET_H
 
 #include <QScrollArea>
+#include <QScrollBar>
+#include <QVBoxLayout>
+#include <QMap>
+#include "commentlistwidgetitem.h"
 
 namespace Ui {
 class CommentListWidget;
@@ -10,9 +14,18 @@ class CommentListWidget;
 class CommentListWidget : public QScrollArea
 {
     Q_OBJECT
+    QWidget* center_widget=nullptr;
+    QVBoxLayout* main_lay=nullptr;
+    QString good_id;
+    QString user_id;
+    bool id_valid;
+    QMap<QString,CommentListWidgetItem*> widgets;
 
 public:
-    explicit CommentListWidget(QWidget *parent = nullptr);
+    void update();
+    bool existCommentSender(const QString& comment_sender)const;
+    CommentListWidgetItem& getItem(const QString& comment_sender);
+    explicit CommentListWidget(const QString& good_id ,const QString& user_id,QWidget *parent = nullptr);
     ~CommentListWidget();
 
 private:
