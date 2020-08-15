37c0e814107a85bd50126f593360055c49afd623
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-15 19:38:42 +0430

    add field name,labels lab<name,rect,space,percent> 2) write function setPercent and static func toQstring

--- Project/Main/Onket/commentitemstatusfield.h
+++ Project/Main/Onket/commentitemstatusfield.h
@@ -3,12 +3,24 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QHBoxLayout>
+#include <QLabel>
 
 class CommentItemStatusField : public QWidget
 {
     Q_OBJECT
+    QString field_name;
+
+    unsigned int percent=0;
+
+    QLabel* lab_name=nullptr,*lab_rect=nullptr,*lab_space=nullptr,*lab_percent=nullptr;
+    QHBoxLayout * main_lay=nullptr;
+
+    void setPercent(unsigned int percent);
+    static QString toQString(unsigned int percent);
 public:
-    explicit CommentItemStatusField(QWidget *parent = nullptr);
+    explicit CommentItemStatusField(const QString& field_name,unsigned int percent,QWidget *parent = nullptr);
+
 
 signals:
 
