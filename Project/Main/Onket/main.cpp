13af1b12b6eaf6f6365d9d3af11e547b907d5822
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-08-19 01:15:26 +0430

    fix confilict

--- Project/Main/Onket/main.cpp
+++ Project/Main/Onket/main.cpp
@@ -1,23 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
-<<<<<<< HEAD
-=======
-#include "type.h"
->>>>>>> 063e114a632d5269e7d937b6ac90562853e23845
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-<<<<<<< HEAD
 
-
-
-=======
-    Type t("موبایل","none");
-
->>>>>>> 063e114a632d5269e7d937b6ac90562853e23845
     MainWindow w;
     w.show();
 
